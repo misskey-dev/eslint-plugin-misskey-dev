bbda34aa665762c66b9a5013ce43acabad87edbc
anatawa12 <anatawa12@icloud.com>
2024-08-16 17:14:11 +0900

    chore: use import-attributes instead of import assertions (#5)

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ import javascript from './configs/javascript.js';
 import recommended from './configs/recommended.js';
 import typescript from './configs/typescript.js';
 
-/**
- * TODO: Import Attributes にしたいけど VS Code の環境によってはエラーになるので今のところ保留
- * @see https://github.com/microsoft/vscode-eslint/issues/1848
- */
-import packageConfig from './package.json' assert { type: 'json' };
+import packageConfig from './package.json' with { type: 'json' };
 
 const plugin = {
 	meta: {
