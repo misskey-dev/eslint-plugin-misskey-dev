890f6ce727e6d004f5116317a9d5b28b37028c57
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-01-10 14:34:06 +0900

    Merge pull request #8 from kakkokari-gtyih/enh-migrate-stylistic
    enhance: `@stylistic/eslint-plugin` に移行

--- configs/javascript.js
+++ configs/javascript.js
@@ -1,37 +1,8 @@
 import { fixupPluginRules } from '@eslint/compat';
+import stylisticEslint from '@stylistic/eslint-plugin';
 import _import from 'eslint-plugin-import';
 
 const jsRules = {
-	'indent': ['warn', 'tab', {
-		'SwitchCase': 1,
-		'MemberExpression': 1,
-		'flatTernaryExpressions': true,
-		'ArrayExpression': 'first',
-		'ObjectExpression': 'first',
-	}],
-	'eol-last': ['error', 'always'],
-	'semi': ['error', 'always'],
-	'semi-spacing': ['error', { 'before': false, 'after': true }],
-	'quotes': ['warn', 'single'],
-	'comma-dangle': ['warn', 'always-multiline'],
-	'comma-spacing': ['error', { 'before': false, 'after': true }],
-	'array-bracket-spacing': ['error', 'never'],
-	'keyword-spacing': ['error', {
-		'before': true,
-		'after': true,
-	}],
-	'key-spacing': ['error', {
-		'beforeColon': false,
-		'afterColon': true,
-	}],
-	'arrow-spacing': ['error', {
-		'before': true,
-		'after': true,
-	}],
-	'brace-style': ['error', '1tbs', {
-		'allowSingleLine': true,
-	}],
-	'padded-blocks': ['error', 'never'],
 	/* TODO: path aliasを使わないとwarnする
 	'no-restricted-imports': ['warn', {
 		'patterns': [
@@ -39,7 +10,6 @@ const jsRules = {
 	}],
 	*/
 	'eqeqeq': ['error', 'always', { 'null': 'ignore' }],
-	'no-multi-spaces': ['error'],
 	'no-var': ['error'],
 	'prefer-arrow-callback': ['error'],
 	'no-throw-literal': ['error'],
@@ -48,30 +18,65 @@ const jsRules = {
 	'no-empty-pattern': ['warn'],
 	'no-async-promise-executor': ['off'],
 	'no-useless-escape': ['off'],
-	'no-multiple-empty-lines': ['error', { 'max': 1 }],
 	'no-control-regex': ['warn'],
 	'no-empty': ['warn'],
 	'no-inner-declarations': ['off'],
 	'no-sparse-arrays': ['off'],
-	'nonblock-statement-body-position': ['error', 'beside'],
-	'object-curly-spacing': ['error', 'always'],
-	'space-infix-ops': ['error'],
-	'space-before-blocks': ['error', 'always'],
-	'padding-line-between-statements': [
-		'error',
-		{ 'blankLine': 'always', 'prev': 'function', 'next': '*' },
-		{ 'blankLine': 'always', 'prev': '*', 'next': 'function' },
-	],
-	'lines-between-class-members': 'off',
 	'import/no-unresolved': ['off'],
 	'import/no-default-export': ['warn'],
 	'import/order': ['warn', {
 		'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
 	}],
+
+	//#region stylistic
+	'@stylistic/array-bracket-spacing': ['error', 'never'],
+	'@stylistic/arrow-spacing': ['error', {
+		'before': true,
+		'after': true,
+	}],
+	'@stylistic/brace-style': ['error', '1tbs', {
+		'allowSingleLine': true,
+	}],
+	'@stylistic/comma-dangle': ['warn', 'always-multiline'],
+	'@stylistic/comma-spacing': ['error', { 'before': false, 'after': true }],
+	'@stylistic/eol-last': ['error', 'always'],
+	'@stylistic/key-spacing': ['error', {
+		'beforeColon': false,
+		'afterColon': true,
+	}],
+	'@stylistic/keyword-spacing': ['error', {
+		'before': true,
+		'after': true,
+	}],
+	'@stylistic/indent': ['warn', 'tab', {
+		'SwitchCase': 1,
+		'MemberExpression': 1,
+		'flatTernaryExpressions': true,
+		'ArrayExpression': 'first',
+		'ObjectExpression': 'first',
+	}],
+	'@stylistic/lines-between-class-members': 'off',
+	'@stylistic/no-multi-spaces': ['error'],
+	'@stylistic/no-multiple-empty-lines': ['error', { 'max': 1 }],
+	'@stylistic/object-curly-spacing': ['error', 'always'],
+	'@stylistic/padded-blocks': ['error', 'never'],
+	'@stylistic/nonblock-statement-body-position': ['error', 'beside'],
+	'@stylistic/padding-line-between-statements': [
+		'error',
+		{ 'blankLine': 'always', 'prev': 'function', 'next': '*' },
+		{ 'blankLine': 'always', 'prev': '*', 'next': 'function' },
+	],
+	'@stylistic/quotes': ['warn', 'single'],
+	'@stylistic/semi': ['error', 'always'],
+	'@stylistic/semi-spacing': ['error', { 'before': false, 'after': true }],
+	'@stylistic/space-before-blocks': ['error', 'always'],
+	'@stylistic/space-infix-ops': ['error'],
+	//#endregion
 };
 
 export default {
 	plugins: {
+		'@stylistic': fixupPluginRules(stylisticEslint),
 		import: fixupPluginRules(_import),
 	},
 	settings: {
